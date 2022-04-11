fdfdb02840b53d7a040eddadab91247115f2dac9
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-04-11 18:42:42 -0300

    build(docker): add shell script for compose

---
+++ scripts/compose-down.sh
@@ -0,0 +1,2 @@
+# sudo sh ./scripts/compose-down.sh
+docker-compose -p 'trybesmith' down --remove-orphans;
---
+++ scripts/compose-up.sh
@@ -0,0 +1,2 @@
+# sudo sh ./scripts/compose-up.sh
+docker-compose -p 'trybesmith' up -d --force-recreate --build;
